28774516cb0a5d9e12f21d61ed709053350e1a2b
Danishvel <79510005+Danishvel@users.noreply.github.com>
2021-10-15 16:40:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function Frame1() {
+    window.location = "index1.html";
+}
+function Frame2() {
+    window.location = "index2.html";
+}
+function Frame3() {
+    window.location = "index3.html";
+}
+function Frame4() {
+    window.location = "index4.html";
+}
+function Frame5() {
+    window.location = "index5.html";
+}
